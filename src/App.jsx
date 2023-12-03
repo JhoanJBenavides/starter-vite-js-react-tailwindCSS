0e8c99d371758e5ede485b1672ec3d9ddabd3586
Jhoan Benavides <jhoanjaime@gmail.com>
2023-12-03 15:03:47 -0500

    starter v1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-center p-2 font-bold">Starter React - Vite - Javascript - TailwindcCSS</h1>
+    </>
+  )
+}
+
+export default App
